0026aaf5734adea8b6702b0544cc34b94d76d4e0
Danila Polevshchikov <dpolevshchikov@ozon.ru>
2021-12-02 01:36:20 +0300

    2021 day 01 solution part 1

---
+++ 2021/2021Day01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/// <summary>
+/// https://adventofcode.com/2021/day/1
+/// count the number of times a depth measurement increases from the previous measurement. (There is no measurement    before the first measurement.) How many measurements are larger than the previous measurement?
+/// </summary>
+
+Console.WriteLine("Day 01 solver");
+
+var deeperMoreCount = 0;
+var deeperLessCount = 0;
+var lastDepth = int.MaxValue;
+foreach (string depthString in File.ReadLines(@"realData.txt"))
+{
+    //Console.WriteLine(depthString);
+    if (int.TryParse(depthString, out int newDepth))
+    { 
+        if (newDepth > lastDepth)           
+        {       
+            deeperMoreCount++;
+        }
+        else
+        {
+            deeperLessCount++;
+        }
+        lastDepth = newDepth;
+    }
+    else
+    {
+        Console.WriteLine($"Error of parsing {depthString}");
+        break;
+    }
+}
+Console.WriteLine($"Answer is {deeperMoreCount} ");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # adventofcode2016
 Small portions of dirty code that solves puzzles from http://adventofcode.com/2016/
+
+update: http://adventofcode.com/2021/ now
